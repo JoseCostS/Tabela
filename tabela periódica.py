e6f2f10c3a03829c3bc9bd573740c24e625b95f7
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-05-15 17:18:46 -0300

    Add files

---
+++ tabela periódica.py
@@ -0,0 +1,35 @@
+qm=input("Digite a Sigla desejada: ")
+qm= qm.lower()
+if qm == "h":
+    print("Nome: Hidrogênio/1")
+    print("Massa: 1,008")
+elif qm == "he":
+    print("Nome: Hélio/2")
+    print("Massa: 4,0026 ")
+elif qm == "li":
+    print("Nome: Lítio/3")
+    print("Massa: 6,941  ")
+elif qm == "be":
+    print("Nome: Berílio/4")
+    print("Massa: 9,0122  ")
+elif qm == "b":
+    print("Nome: Boro/5")
+    print("Massa: 10,81 ")
+elif qm == "c":
+    print("Nome: Carbono/6")
+    print("Massa: 12,001 ")
+elif qm == "n":
+    print("Nome: Nitrogênio/7")
+    print("Massa: 14,004 ")
+elif qm == "o":
+    print("Nome: Oxigênio/8")
+    print("Massa: 15,999")
+elif qm == "f":
+    print("Nome: Flúor/9")
+    print("Massa: 18,998 ")
+elif qm == "Ne":
+    print("Nome: Neónio/10")
+    print("Massa: 20,180 ")
+else:
+    print("A sigla digitada não existe")
+
